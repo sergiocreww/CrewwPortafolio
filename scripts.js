05765c595d9462cb35252693c05d37393dd31382
sergiocreww <sssssscccccc111@gmail.com>
2024-07-20 12:53:02 -0500

    segundo commit

--- scripts.js
+++ scripts.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('form');
     form.addEventListener('submit', function(event) {
         event.preventDefault();
-        alert('¡Gracias por tu mensaje!');
+        alert('¡Gracias por comunicarte con Sergiocreww!, Estare en contacto contigo lo mas pronto posible');
     });
 });
